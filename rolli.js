67e0a17b5cb4ec6d881e8b2c80cdf9d0a31ee1e6
Paul Reimer <paulreimer@gmail.com>
2011-03-13 12:51:17 -0700

    Retabbed, simplified rolli.js

--- rolli.js
+++ rolli.js
@@ -1,46 +1,31 @@
-
-function playerUpdate() {
-	var oldPlayState = 0;
-	var pState = iTunes.playState();
-	if (pState != oldPlayState)
-	{
-		if (pState == 0 || pState == 2) {
-			$('#infoBox').fadeTo(400, 0.4);
-			$('#art').fadeTo(400, 0.4);
-		}
-		else {
-			$('#infoBox').fadeTo(400, 1);
-			$('#art').fadeTo(400, 1);
-		}
-		oldPlayState = pState;
-	}
+function playerUpdate()
+{
+  var oldPlayState = 0;
+  var pState = iTunes.playState();
+  if (pState != oldPlayState)
+  {
+    if (pState == 0 || pState == 2)
+    {
+      $('#infoBox').fadeTo(400, 0.4);
+      $('#art').fadeTo(400, 0.4);
+    }
+    else {
+      $('#infoBox').fadeTo(400, 1);
+      $('#art').fadeTo(400, 1);
+    }
+    oldPlayState = pState;
+  }
 }
 
-function trackUpdate(track){
-		$('#title').html(track.property('title'));
-			if ('title' == ""){
-				$('#title').attr('');
-			}		
-		$('#artist').html(track.property('artist'));
-			if ('artist' == ""){
-				$('#artist').attr('');
-			}
-		$('#album').html(track.property('album'));
-			if ('album' == ""){
-				$('#album').attr('');
-			}
+function trackUpdate(track)
+{
+  $('#title'  ).html(track.property('title')  || '');
+  $('#artist' ).html(track.property('artist') || '');
+  $('#album'  ).html(track.property('album')  || '');
 }
 
-function artworkUpdate(artURL){
-		if (artURL == ""){
-			$('#art').attr({ 
-        		src: "defaultartwork.png"
-        	});
-		}
-		else
-		{
-			$('#art').attr({ 
-        		src: artURL
-        	});
-		}
+function artworkUpdate(artURL)
+{
+  artURL = artURL || 'defaultartwork.png';
+  $('#art').attr({ src: artURL });
 }
